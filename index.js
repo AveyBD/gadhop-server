14ea91b673a2f72f60b6ece1f6990101e57e94cf
Debashish D. Dev <hello@debashish.dev>
2022-05-03 14:41:30 +0600

    App Working

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// Root API
+app.get("/", (req, res) => {
+  res.send(`App is working. Last Reporting Time: ${Date()}`);
+});
+app.listen(port);
