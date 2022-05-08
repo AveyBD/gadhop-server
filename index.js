be63af32438f62cb59505a068346d63596f89fd0
Debashish D. Dev <hello@debashish.dev>
2022-05-08 12:49:11 +0600

    Database Connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,10 +9,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// connetion to mongodb
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.14iau.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("products").collection("userProducts");
+  console.log('Database Connected Successfully');
+  // perform actions on the collection object
+  client.close();
+});
+
 // Root API
 app.get("/", (req, res) => {
   res.send(`App is working. Last Reporting Time: ${Date()}`);
 });
-app.listen(port,()=>{
-    console.log(`App Listening to port: ${port}`);
+app.listen(port, () => {
+  console.log(`App Listening to port: ${port}`);
 });
