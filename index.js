27327917df2db0821394ef34bf783d18ecdfa142
Debashish D. Dev <hello@debashish.dev>
2022-05-08 15:29:22 +0600

    Calling all product working

--- index.js
+++ index.js
@@ -10,20 +10,30 @@ app.use(cors());
 app.use(express.json());
 
 // connetion to mongodb
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.14iau.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.14iau.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("products").collection("userProducts");
-  console.log('Database Connected Successfully');
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("products").collection("addedItems");
+
+    // get all product api
+    app.get("/manage", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const product = await cursor.toArray();
+      res.send(product)
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 // Root API
 app.get("/", (req, res) => {
   res.send(`App is working. Last Reporting Time: ${Date()}`);
