0116661c29013cea8bd080c354f3e83b1b8ac273
Debashish D. Dev <hello@debashish.dev>
2022-05-09 14:58:57 +0600

    fixed Quantity

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ async function run() {
       const options = { upsert: true };
       const updatedDoc = {
         $set: {
-          quantity: updatedQty,
+          quantity: updatedQty.quantity,
         },
       };
       const result = await productCollection.updateOne(
